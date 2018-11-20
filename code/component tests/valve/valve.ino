d446765230e44ab3090fee3ec94b1e4632392461
Mingke Yu <mingke.yu.miko@gmail.com>
2018-11-20 09:14:22 -0800

    valve test - cotrol a valve with Arduio

--- code/component tests/valve/valve.ino
+++ code/component tests/valve/valve.ino
@@ -1,19 +1,27 @@
-int solenoidPin = 4; //solenid valve pin
+int solenoidPin = 9; //solenid valve pin
+int pushButton = 2;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(solenoidPin, OUTPUT); 
+  pinMode(solenoidPin, OUTPUT);
+  pinMode(pushButton, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(solenoidPin, HIGH); // switch the valve ON
-  Serial.println("valve ON");
-  delay (10000);
-  
-  digitalWrite(solenoidPin, LOW); // switch the valve OFF
-  Serial.println("valve OFF");
-  delay (10000);
-  
+
+  // read the state of the button and check if it is pressed
+  if (digitalRead(pushButton) == HIGH) {
+    digitalWrite(solenoidPin, HIGH); // switch the valve ON
+    Serial.println("valve ON");
+    //delay (500);
+  } else {
+
+    // ramp down the motor speed
+    digitalWrite(solenoidPin, LOW); // switch the valve OFF
+    Serial.println("valve OFF");
+    //delay (500);
+  }
+
+  delay(1);        // delay in between reads for stability
 }
