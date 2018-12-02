10d95f859804c0133cb9cb7ff2be1176b5d1c6e1
Mingke Yu <mingke.yu.miko@gmail.com>
2018-12-02 15:58:25 -0800

    testing code for 6 valves updated

--- code/fortuneTelling_v2/machine_valves/machine_valves.ino
+++ code/fortuneTelling_v2/machine_valves/machine_valves.ino
@@ -1,37 +1,46 @@
 const int valvePins[] = {8, 9, 10, 11, 12, 13};
-const int pushButton = 2;
 
-int workingValve = 0;
-//int randomValve = 0;
+int workingValve = 1;
+int workingPin = 8;
+int pushButton = 2;
 
 void setup() {
-  // define the in/out-put pins
   for (int i = 0; i < 6; i++) {
-    pinMode(valvePins[i + 8], OUTPUT);
+    pinMode(valvePins[i], OUTPUT);
   }
+  pinMode(workingPin, OUTPUT);
   pinMode(pushButton, INPUT);
-
   Serial.begin(9600);
-  Serial.println("Start");
 }
 
 void loop() {
 
   // read the state of the button and check if it is pressed
   if (digitalRead(pushButton) == HIGH) {
-    //    randomValve = int(rand(8, 14));
-    //    workingValve = randomValve;
-    
-    digitalWrite(valvePins[workingValve], HIGH); // switch the valve ON
-    Serial.print("valve no."); Serial.print(workingValve); Serial.println("ON");
+
+    workingValve = int(random(1, 7));
+    Serial.print("valve no. "); Serial.print(workingValve); Serial.print(" ON");
+    workingPin = valvePins[workingValve - 1];
+    Serial.print(" pin no. "); Serial.print(workingPin);
+    Serial.println();
+    digitalWrite(workingPin, HIGH); // switch the valve ON
     delay (6500);
-    workingValve ++;
+
+    /*
+    // loop by order for test
+    if (workingValve < 6) {
+      workingValve ++;
+    } else {
+      workingValve = 1;
+    }
+    Serial.print("next valve no. "); Serial.println(workingValve);
+    */
 
   } else {
-    for (int i = 0; i < 6; i++) {
-      digitalWrite(valvePins[i + 8], LOW); // all valves OFF
-    }
-    Serial.println("valves OFF");
+
+    // ramp down the motor speed
+    digitalWrite(workingPin, LOW); // switch the valve OFF
+    Serial.println("valve OFF");
     //delay (500);
   }
 
