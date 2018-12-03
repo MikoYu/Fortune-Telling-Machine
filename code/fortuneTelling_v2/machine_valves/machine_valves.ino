1a004d63d93161fca195d56817d7e30f988f4624
Mingke Yu <mingke.yu.miko@gmail.com>
2018-12-02 16:32:41 -0800

    add random seed for valve control

--- code/fortuneTelling_v2/machine_valves/machine_valves.ino
+++ code/fortuneTelling_v2/machine_valves/machine_valves.ino
@@ -1,15 +1,25 @@
+// set up constant pins
 const int valvePins[] = {8, 9, 10, 11, 12, 13};
+const int pushButton = 2;
 
+// set up variables for the working valve
 int workingValve = 1;
 int workingPin = 8;
-int pushButton = 2;
+
+// for random seed
+const int seedPin = A5;
 
 void setup() {
+  // set up pin modes
   for (int i = 0; i < 6; i++) {
     pinMode(valvePins[i], OUTPUT);
   }
   pinMode(workingPin, OUTPUT);
   pinMode(pushButton, INPUT);
+
+  // use the input on A5 as random seed, to make the selection seem more "random"
+  randomSeed(analogRead(seedPin));
+  
   Serial.begin(9600);
 }
 
@@ -23,6 +33,8 @@ void loop() {
     workingPin = valvePins[workingValve - 1];
     Serial.print(" pin no. "); Serial.print(workingPin);
     Serial.println();
+
+    // switch on working pin
     digitalWrite(workingPin, HIGH); // switch the valve ON
     delay (6500);
 
